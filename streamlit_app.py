89d589ca4bd75e68fd41c0216fb4d0e5e5ded6b1
AMIROLIMI <oamir3867@mail.ru>
2024-10-05 16:54:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('First APP of Amir')
 st.write('Hello world!')
 
 with st.expander('Initial data'):
-  st.write('**RAw data**')
+ # st.write('**RAw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
 st.write('**X**')
@@ -15,4 +15,7 @@ x_raw
 st.write('**y**')
 y_raw = df.species
 y_raw
+
+with st.expander('Data vizualization'):
+  st.scater_chart(data=df, x='bill_length_nn', y='body_nass_g', color='species')
   

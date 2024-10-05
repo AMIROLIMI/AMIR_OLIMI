3de4a26ed8c5c2752691b353a8748f9f02ed661f
AMIROLIMI <oamir3867@mail.ru>
2024-10-05 16:48:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,16 @@ import streamlit as st
 st.title('First APP of Amir')
 
 st.write('Hello world!')
+
+with st.expander('Initial data'):
+  st.write('**RAw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+st.write('**X**')
+X_raw = df.drop('species', axis=1)
+x_raw
+
+st.write('**y**')
+y_raw = df.species
+y_raw
+  
